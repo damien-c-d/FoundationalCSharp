e1158dd0e938de7d8d35520e89893e676321f80b
Damien Dussart <damien.dussart@outlook.com.au>
2023-11-25 14:14:16 +1100

    first commit

---
+++ CompleteTheChallenge/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: Write code in the .NET Editor to display two messages
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+//Async Example not using top level statements
+// internal class Program
+// {
+//     static async Task Main(string[] args)
+//     {
+//         await Console.Out.WriteLineAsync("Hello, World!");
+//     }
+// }
+
+
+//Display a new message
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
