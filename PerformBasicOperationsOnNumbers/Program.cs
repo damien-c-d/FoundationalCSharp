74b66be222574673d6c9305376c41982aa2aae81
Damien Dussart <damien.dussart@outlook.com.au>
2023-11-25 22:20:03 +1100

    PerformBasicOperationsOnNumbers

---
+++ PerformBasicOperationsOnNumbers/Program.cs
@@ -0,0 +1,88 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// int firstNumber = 12;
+// int secondNumber = 7;
+// Console.WriteLine(firstNumber + secondNumber);
+
+// string firstName = "Bob";
+// int widgetsSold = 7;
+// Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
+
+// string firstName = "Bob";
+// int widgetsSold = 7;
+// Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
+
+// string firstName = "Bob";
+// int widgetsSold = 7;
+// Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+// int sum = 7 + 5;
+// int difference = 7 - 5;
+// int product = 7 * 5;
+// int quotient = 7 / 5;
+
+// Console.WriteLine("Sum: " + sum);
+// Console.WriteLine("Difference: " + difference);
+// Console.WriteLine("Product: " + product);
+// Console.WriteLine("Quotient: " + quotient);
+
+// decimal decimalQuotient = 7.0m / 5;
+// decimal decimalQuotient = 7 / 5.0m;
+// decimal decimalQuotient = 7.0m / 5.0m;
+// Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+
+// int first = 7;
+// int second = 5;
+// decimal quotient = (decimal)first / (decimal)second;
+// Console.WriteLine(quotient);
+
+// Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+// Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
+// int value1 = 3 + 4 * 5;
+// int value2 = (3 + 4) * 5;
+// Console.WriteLine(value1);
+// Console.WriteLine(value2);
+
+// int value = 0;     // value is now 0.
+// value = value + 5; // value is now 5.
+// value += 5;        // value is now 10.
+// System.Console.WriteLine(value);
+
+// int value = 1;
+
+// value = value + 1;
+// Console.WriteLine("First increment: " + value);
+
+// value += 1;
+// Console.WriteLine("Second increment: " + value);
+
+// value++;
+// Console.WriteLine("Third increment: " + value);
+
+// value = value - 1;
+// Console.WriteLine("First decrement: " + value);
+
+// value -= 1;
+// Console.WriteLine("Second decrement: " + value);
+
+// value--;
+// Console.WriteLine("Third decrement: " + value);
+
+// int value = 1;
+// value++;
+// Console.WriteLine("First: " + value);
+// Console.WriteLine($"Second: {value++}");
+// Console.WriteLine("Third: " + value);
+// Console.WriteLine("Fourth: " + (++value));
+
+
+// int farenheit = 94;
+// decimal celsius = (farenheit - 32m) * (5m / 9m);
+// Console.WriteLine($"The temperature is {celsius} Celsius.");
+
+System.Console.WriteLine(5 / 10); // Will print 0 - 0.5 rounded down as it is an int
+
+int result = 3 + 1 * 5 / 2;
+System.Console.WriteLine(result); // Will print 5. 5 / 2 = 2.5 rounded down (because int) to 2 * 1 = 2 + 3 = 5
